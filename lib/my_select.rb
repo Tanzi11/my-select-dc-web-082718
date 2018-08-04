92244d25c18c0076fcf904400ab1f7b0ca0c4400
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-08-04 20:09:15 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,3 @@
 nums = [1, 2, 3, 4, 5] 
 def my_select(collection)
- empty_arr = [] 
  
- i=0 
- while i < collection.length
- empty_arr.push yield(collection[i]) 
- i += 1
- end
- 
- empty_arr.compact  
-end
